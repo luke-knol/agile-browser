98597f31896ca66b10390aad65ad67b61c8ce86b
luke-knol <luke.knol@gmail.com>
2013-08-28 18:54:56 -0400

    updates to allow multiple users with custom egress paths

--- classes/php/auth.php
+++ classes/php/auth.php
@@ -5,40 +5,46 @@ include "includes/uploadAPI/uploadAPI.php";
 
 class Auth
 {
-	private $uploadAPI;
-	private $pathPrefix;
-	private $uploadURI;
+        private $uploadAPI;
+        private $pathPrefix;
+        private $uploadURI;
 
-	public function Login($user, $pass){
-		$obj = simplexml_load_file('/etc/agile/browser_config.xml');
-		$uploadUriNodes = $obj->xpath('/agile/service/uploadAPI');
-		$pathPrefixNode = $obj->xpath('/agile/service/pathPrefix');
-		$envPrefixNode = $obj->xpath('/agile/service/envPrefix');
-		$this->uploadURI = (string)$uploadUriNodes[0][0];
-		$this->pathPrefix = (string)$pathPrefixNode[0][0];
-		$_SESSION['envPrefix'] = (string)$envPrefixNode[0][0];
-		if(strpos($this->pathPrefix,'%username') >= 0){
-			$this->pathPrefix = str_replace('%username', $user, $this->pathPrefix);
-		}
-		if(substr($this->pathPrefix, -1) == '/'){
-			$this->pathPrefix = rtrim($this->pathPrefix, '/');
-		}
-		$this->uploadAPI = new UploadAPI($this->uploadURI);
-		$r = $this->uploadAPI->login($user, $pass);
-		return $r;
-	}
+        public function Login($user, $pass){
+                $obj = simplexml_load_file('/etc/agile/browser_config.xml');
+                $uploadUriNodes = $obj->xpath('/agile/service/uploadAPI');
+                $envPrefixNode = $obj->xpath('/agile/service/envPrefix');
+                $pathPrefixNode = $obj->xpath('/agile/userPrefix/'.$user);
+                $this->uploadURI = (string)$uploadUriNodes[0][0];^M
+                if(count($pathPrefixNode) == 1){
+                        $this->pathPrefix = (string)$pathPrefixNode[0][0];
+                }
+                else{
+                        $pathPrefixNode = $obj->xpath('/agile/service/pathPrefix');
+                        $this->pathPrefix = (string)$pathPrefixNode[0][0];
+                        if(strpos($this->pathPrefix,'%username') >= 0){^M
+                                $this->pathPrefix = str_replace('%username', $user, $this->pathPrefix);^M
+                        }
+                }
+                $_SESSION['envPrefix'] = (string)$envPrefixNode[0][0];^M
+                if(substr($this->pathPrefix, -1) == '/'){^M
+                        $this->pathPrefix = rtrim($this->pathPrefix, '/');^M
+                }
+                $this->uploadAPI = new UploadAPI($this->uploadURI);^M
+                $r = $this->uploadAPI->login($user, $pass);^M
+                return $r;
+        }
 
-	public function GetUploadAPI(){
-		return $this->uploadAPI;
-	}
+        public function GetUploadAPI(){
+                return $this->uploadAPI;
+        }
 
-	public function GetPathPrefix(){
-		return $this->pathPrefix;
-	}
+        public function GetPathPrefix(){^M
+                return $this->pathPrefix;^M
+        }
 
-	public function GetUploadURI(){
-		return $this->uploadURI;
-	}
+        public function GetUploadURI(){
+                return $this->uploadURI;
+        }
 }
 
 ?>
